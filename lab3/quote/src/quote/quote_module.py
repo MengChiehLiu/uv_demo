415fc01f78842647d68788f5b01db95c0eacf4b3
Jack Liu <jack.liu@ibm.com>
2025-02-24 16:23:34 +0800

    fix: simplified chinese to traditional chinese

--- lab3/quote/src/quote/quote_module.py
+++ lab3/quote/src/quote/quote_module.py
@@ -1,14 +1,13 @@
 import requests
-import opencc
+import zhconv
 
-converter = opencc.OpenCC('s2t')
 
 def get_quote():
     """A function to get poisoned chicken soup through api"""
     try:
         url = "https://api.oick.cn/dutang/api.php"
         simplified_text = requests.get(url).text
-        return converter.convert(simplified_text)
+        return zhconv.convert(simplified_text, 'zh-tw')
     except:
         return "三百六十行，行行出BUG。"
 
